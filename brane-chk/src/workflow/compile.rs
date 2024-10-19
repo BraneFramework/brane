378e5cf6b9cd26a9bef540903c24a9a60e54e9b5
Lut99 <mullertim99@gmail.com>
2024-10-19 11:25:30 +0200

    Added compiler to eFLINT
    Mind you, is untested

--- brane-chk/src/workflow/compile.rs
+++ brane-chk/src/workflow/compile.rs
@@ -4,7 +4,7 @@
 //  Created:
 //    27 Oct 2023, 17:39:59
 //  Last edited:
-//    18 Oct 2024, 11:47:15
+//    19 Oct 2024, 11:07:29
 //  Auto updated?
 //    Yes
 //
@@ -257,7 +257,7 @@ fn reconstruct_graph(
             // Return the elem
             Ok(Elem::Call(ElemCall {
                 id: format!("{}-{}-task", wf_id, pc.resolved(&wir.table)),
-                task: format!("{}::{}::[{}]", def.function.name, def.package, def.version),
+                task: format!("{}[{}]::{}", def.package, def.version, def.function.name),
                 input: input
                     .iter()
                     .map(|(name, avail)| Dataset {
